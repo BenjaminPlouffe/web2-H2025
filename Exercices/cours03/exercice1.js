cbd1000e607de65a9e7729265021e34f841429a6
BenjaminPlouffe <plouffebenjamin17@gmail.com>
2025-02-04 09:44:01 -0500

    Create exercice1.js

---
+++ Exercices/cours03/exercice1.js
@@ -0,0 +1,30 @@
+//Benjamin Plouffe
+//2025-02-04
+
+import ps from 'prompt-sync';
+const prompt = ps();
+
+//Exercice 1 : Objets JavaScript
+
+function Produit(_nom='Inconnu', _prix=0) {
+    this.nom = _nom;
+    this.prix = _prix;
+}
+
+const produit1 = new Produit("Ordinateur", 1200);
+const produit2 = new Produit("Clavier", 100);
+const produit3 = new Produit("Moniteur", 400);
+const produit4 = new Produit("Souris", 50);
+
+
+// a
+const tabProduits =[produit1,produit2,produit3,produit4,] 
+
+// b
+delete produit2.prix;
+produit2.prix = "200";
+console.log(produit2);
+
+// c
+let sommeProduitsTotal =tabProduits.reduce((sommeProduits, element) => parseInt(sommeProduits) + parseInt(element.prix), 0);
+console.log(sommeProduitsTotal);
