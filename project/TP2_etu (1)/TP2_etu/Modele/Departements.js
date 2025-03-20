0b7a02a60c2ab1dae61516369eb980c5f60ef8f9
BenjaminPlouffe <plouffebenjamin17@gmail.com>
2025-03-20 15:19:44 -0400

    tp2 debut

--- project/TP2_etu (1)/TP2_etu/Modele/Departements.js
+++ project/TP2_etu (1)/TP2_etu/Modele/Departements.js
@@ -1,26 +1,4 @@
 // Tableau des départements (à importer dans le modèle)
-class Employee {
-
-constructor(matricule ='N/D',prenom='N/D',nom='N/D',salaire=0,departement='N/D',retraite = false,dateDeRetraite = 0) {
-    this.matricule = matricule;
-    this.prenom = prenom;
-    this.nom = nom;
-    this.salaire = salaire;
-    this.departement = departement;
-    this.retraite = retraite;
-    this.dateDeRetraite =dateDeRetraite;
-    
-}
-
-}
-
-class Employees{
-    constructor(list = []){
-        this.list = list;
-    }
-}
-
-
 
 const _departements = [
     { id: 1, nom: 'Comptabilité' },
@@ -30,4 +8,4 @@ const _departements = [
     { id: 5, nom: 'Ressources humaines' },
 ];
 
-export default _departements + Employee;
+export default _departements
---
+++ project/TP2_etu (1)/TP2_etu/Modele/Employees.js
@@ -0,0 +1,11 @@
+import { Employee } from "./Employee";
+
+class Employees{
+    constructor(list = []){
+        this.list = list;
+    }
+
+}
+
+
+export default Employees
